43f95ae39d1e2130b4057b9a210f1ac5fe46856e
denBot <den.xyz@protonmail.com>
2018-06-30 23:08:11 +0100

    Updated FFMPEG Source generator

--- easy_thumbnails_ffmpeg/source_generators.py
+++ easy_thumbnails_ffmpeg/source_generators.py
@@ -1,35 +1,34 @@
-# -*- coding: utf-8 -*-
+from django.utils.six import BytesIO
+import os
 from subprocess import Popen, PIPE
-
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-
+from PIL import Image
+from random import choices
+from string import ascii_lowercase, ascii_uppercase
 
 def ffmpeg_frame(source, frame='00:00:01', **options):
-    """
-    Try to load a frame of the given video with ffmpeg, ignoring all errors.
-    """
-    if not source or not source._file:
+
+    if not source:
         return
-    source._file.seek(0)
-    data = Popen([
-        'ffmpeg', '-i', '-', '-an',
-        '-vcodec', 'png', '-vframes', '1',
-        '-ss', frame, '-y',
-        '-f', 'rawvideo', '-'
-    ], stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate(source.read())[0]
+    # Generates random filename for data stream and writes stream to temp-file
+    filename = ''.join(choices(ascii_lowercase + ascii_uppercase, k=8))
+    with open(filename, 'wb') as f:
+        f.write(source.read())
 
-    if not data:
+    try:
+        # Opens file with ffmpeg, grabs frame at 1 second & removes tempfile
+        cmd = ['ffmpeg', '-i', filename, '-ss', frame, '-vframes', '1', '-f', 'image2', 'pipe:1']
+        data = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate(source.read())
+        os.remove(filename)
+        if not data[0]:
+            return
+    except (Exception, IndexError, FileNotFoundError):
         return
-
-    image = Image.open(StringIO(data))
-    image.load()
-
+    
+    image = Image.open(BytesIO(data[0]))
+    try:
+        image.load()
+    except IOError:
+        pass
+    
+    image = utils.exif_orientation(image)
     return image
